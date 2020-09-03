6e629e1179f19e7179879eaf154b28add337c1a5
RichardJeanBaptiste <Richinbk1@gmail.com>
2020-09-03 07:34:51 -0400

    asd

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ try {
     db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 }
 
-//main view
+//main view 
 app.get('/', async function(req,res){
     res.sendFile(__dirname + "/views/index.html")
 })

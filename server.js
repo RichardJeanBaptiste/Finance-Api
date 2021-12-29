4dcca38e5e2b2ee7d4ac34f53263e60ea1ef6342
Richard Jean-Baptiste <Richinbk1@gmail.com>
2021-12-29 17:49:51 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,148 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const helmet = require('helmet');
+const cors = require('cors');
+const Quote = require('./models/qoute.models');
+const {Login, Admin} = require('./models/login.models');
+const loginSchema = require('./Schemas/login.schema');
+const bcrypt =  require('bcryptjs');
+const app = express();
+const PORT = process.env.PORT || 4000;
+const quoteRoute = require('./routes/quoteRoutes');
+const AdminRoutes = require('./routes/AdminRoutes');
+const ListRoutes = require('./routes/ListRoute');
+const create = require('./create');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
+const flash = require('connect-flash');
+const timeout = require('connect-timeout');
+const morgan = require('morgan');
+const saltRounds = 10;
+const MONGOURI = process.env.MONGO_URI || "mongodb+srv://Richinbk:VZUbwFmW3d4EUSjw@finance-api.jvol5.mongodb.net/Finance-Quotes?retryWrites=true&w=majority";
 
+
+// connect to database
+try {
+    mongoose.connect(MONGOURI, {useNewUrlParser: true, useUnifiedTopology:true}).catch((err)=> {
+        console.log(err)
+    });
+} catch (error) {
+    let db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+}
+
+const store = new MongoDBStore({
+    uri: MONGOURI,
+    collection: 'sessions'
+});
+
+// Catch Store Errors
+store.on('error', function(error) {
+    //console.log('Store Error')
+    console.log(error);
+});
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+  
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({ 
+    secret: process.env.SECRET_KEY || 'secret cat',
+    resave: true,
+    saveUninitialized: false,
+    cookie: 
+        { 
+            secure: true, 
+            maxAge: 3600000,  
+        },
+    store: store,
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(errorHandler)
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        imgSrc: ["'self'"],
+        scriptSrc: [" 'self'", "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"],
+        styleSrc: ["'self'", 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css'],
+    }
+}))
+app.use(morgan('combined'));
+app.use(cors());
+
+//aDADsaf
+
+
+app.use('/admin', AdminRoutes);
+app.use('/quotes', quoteRoute);
+app.use('/list', ListRoutes);
+
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+
+
+function errorHandler(err, req, res, next){
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+}
+
+
+/*
+
+app.post('/addquotes', async function(req,res){
+    try {
+
+        let author = req.body.author;
+        let count = Object.keys(req.body).length;
+
+        if(count === 1){
+            res.send("Empty Params");
+        }else{
+
+            for(let key in req.body){
+                if(req.body[key] != author){
+                    //console.log(req.body[key]);
+                    create(author,req.body[key]);
+                }
+            }
+            res.send("Quotes Added");
+        }
+        
+    } catch (error) {
+        
+        res.send(error);
+    }
+    
+});
+
+*/
+
+function haltOnTimedout (req, res, next) {
+    if(!req.timedout) next()
+}
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`)
+})

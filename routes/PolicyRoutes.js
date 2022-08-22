95198342c40ffd642667781f9999906e9e509da3
Richard Jean-Baptiste <Richinbk1@gmail.com>
2022-08-22 16:24:59 -0400

    cleaning

--- routes/PolicyRoutes.js
+++ routes/PolicyRoutes.js
@@ -8,7 +8,6 @@ router.get('/privacy', async function(req, res){
 
 router.get('/terms', async function(req, res){
     res.sendFile(path.join(__dirname + '/../views/terms.html'));
-    //res.redirect('https://www.termsfeed.com/live/a64b54c9-eb12-4f09-8d50-67c9cf78a4c0')
 })
 
 

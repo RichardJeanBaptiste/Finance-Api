96a18bcce837523714afc95490112b684005f915
Richard Jean-Baptiste <Richinbk1@gmail.com>
2022-09-02 16:20:25 -0400

    change admin

--- routes/EditRoutes.js
+++ routes/EditRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-//const quoteSchema = require('../Schemas/quote.schema');
 const MONGOURI = process.env.MONGO_URI;
 
 router.post('/addquotes', async function(req,res){
